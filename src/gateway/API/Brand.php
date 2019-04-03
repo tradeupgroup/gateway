1b58d4bcc6d9c4ba090e8932342081d421514d27
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-04-03 14:41:59 -0300

    Add getConstants in brand class

--- src/gateway/API/Brand.php
+++ src/gateway/API/Brand.php
@@ -1,30 +1,37 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: brunopaz
-     * Date: 2018-12-28
-     * Time: 01:27
-     */
+/**
+ * Created by PhpStorm.
+ * User: brunopaz
+ * Date: 2018-12-28
+ * Time: 01:27
+ */
 
-    namespace Gateway\API;
+namespace Gateway\API;
 
 
-    abstract class Brand
+use ReflectionClass;
+
+abstract class Brand
+{
+    const VISA = "visa";
+    const MASTERCARD = "mastercard";
+    const DINERS = "diners";
+    const DISCOVER = "discover";
+    const ELO = "elo";
+    const AMEX = "amex";
+    const AURA = "aura";
+    const JCB = "jcb";
+    const HYPERCARD = "hypercard";
+    const SOROCRED = "sorocred";
+    const CABAL = "cabal";
+    const MAESTRO = "maestro";
+    const HIPER = "hiper";
+    const CREDSYSTEM = "credsystem";
+    const BANESCARD = "banescard";
+    const CREDZ = "credz";
+
+    public static function getConstants()
     {
-        const VISA       = "visa";
-        const MASTERCARD = "mastercard";
-        const DINERS     = "diners";
-        const DISCOVER   = "discover";
-        const ELO        = "elo";
-        const AMEX       = "amex";
-        const AURA       = "aura";
-        const JCB        = "jcb";
-        const HYPERCARD  = "hypercard";
-        const SOROCRED   = "sorocred";
-        const CABAL      = "cabal";
-        const MAESTRO    = "maestro";
-        const HIPER      = "hiper";
-        const CREDSYSTEM = "credsystem";
-        const BANESCARD  = "banescard";
-        const CREDZ      = "credz";
+        return (new ReflectionClass(__CLASS__))->getConstants();
     }
+}

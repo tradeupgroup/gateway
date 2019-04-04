2900aed00ace5e00be5e4539b97a7ceaa1c79a05
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-04-04 13:57:08 -0300

    Remove unset cpf in tokenization

--- src/gateway/API/Tokenization.php
+++ src/gateway/API/Tokenization.php
@@ -34,8 +34,6 @@
         public function __construct(Credential $credential, Card $card, Customer $customer)
         {
             $request = new Request($credential);
-            $customer = array_filter($customer->jsonSerialize());
-            unset($customer['cpf']);
             $json["transaction-request"] = [
                 "version"      => "1.0.0",
                 "verification" => [
